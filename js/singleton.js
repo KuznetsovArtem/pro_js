a7a07dd157a6d31a3b66d132f412c8eb0ba53779
Artem <murdoc.ak@gmai.com>
2014-03-03 12:57:30 +0200

    singleton  pattern

---
+++ js/singleton.js
@@ -0,0 +1,41 @@
+/**
+ * Singleton
+ */
+var singleton = (function() {
+
+    var instance;
+
+    function init() {
+        // Singleton body
+        function privateMethod() {
+            console.log('from private');
+        }
+        var privateVar = 'private var text';
+        var privateRandom = Math.random();
+        return {
+            publicMethid: function() {
+                console.log("from public");
+            },
+            publicProperty: "public prop test",
+            getRandom: function() {
+                return privateRandom;
+            }
+
+        }
+    }
+
+    return {
+        getInstance: function() {
+            if(!instance) {
+                instance = init()
+            }
+            return instance;
+        }
+    };
+})();
+
+var singleA = singleton.getInstance();
+var singleB = singleton.getInstance();
+
+
+// TODO: one more example
